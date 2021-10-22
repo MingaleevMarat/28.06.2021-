e665e1485000e1288256a50270fa5198b7502e3b
Ming_tc51 <Ming_tc51@itstepru.onmicrosoft.com>
2021-10-22 11:10:50 +0500

    Добавьте файлы проекта.

---
+++ 28.06.2021 урок нов/28.06.2021 урок нов.cpp
@@ -0,0 +1,88 @@
+﻿// 28.06.2021 урок нов.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	//srand(time(NULL));
+//	//int matrix[3][3];
+//	//int sum = 0;
+	//for (size_t i = 0; i < 3; i++)
+	//	for (size_t j = 0; j < 3; j++)
+	//		matrix[i][j] = rand() % 27 - 12;
+	//for (size_t i = 0; i < 3; i++)
+	//{
+	//	for (size_t j = 0; j < 3; j++)
+	//		cout << matrix[i][j] << " ";
+	//	cout << endl;
+	//}
+	//for (size_t i = 0; i < 3; i++)
+	//	for (size_t j = 0; j < 3; j++)
+	//		sum += matrix[i][j];
+	//cout << "Sum of matrix elements: " << sum << "\n";
+	//int proiz = 1;
+	//for (size_t i = 0; i < 3; i++)
+	//	for (int j = 0; j < 3; j++)
+	//		proiz *= matrix[i][j];
+	//cout << "Proizvedenie of matrix elements: " << proiz;
+	//int elem = 0, el = 0;
+	//cout << "Enter element,please: ";
+	//cin >> elem;
+	//for (size_t i = 0; i < 3; i++)
+	//	for (int j = 0; j < 3; j++)
+	//		//while (elem == matrix[i][j]) Я сначала вот так вместе с if написал.
+	//		if (elem == matrix[i][j])
+	//			++el;
+	//cout << "Kolichestvo of matrix elements: " << el;
+	//int ele = 0;
+	//cout << "Enter number element: \n";
+	//cin >> ele;
+	//bool contains = false;
+	//for (size_t i = 0; i < 3; i++)
+	//	for (int j = 0; j < 3; j++)
+	//		if (ele == matrix[i][j])
+	//			contains = true;
+	//cout << "Element contains in matrix?" << (contains == true ? "Yes" : "No");
+	//cout << endl;
+	//long long max = 0;
+	//for (size_t i = 0; i < 3; i++)
+	//	for (int j = 0; j < 3; j++)
+	//	{
+	//		if (max < matrix[i][j])
+	//			max = matrix[i][j];
+	//	}
+	//cout << "max" << max << endl;
+
+	//int min = 0;
+	//for (size_t i = 0; i < 3; i++)
+	//	for (int j = 0; j < 3; j++)
+	//	{
+	//		if (min > matrix[i][j])
+	//			min = matrix[i][j];
+	//	}
+	//cout << "min" << min << endl;
+	//sum = 0;
+	//for (size_t i = 0; i < 3; i++)
+	//	sum += matrix[i][i];
+	//cout << "Sum of elements of main diagonal: " << sum;
+
+	//return 0;
+
+
+
+	/*int num = 0, nu = 0;
+	cout << "Vvedite num: ";
+	cin >> num;
+	while (num > 0)
+	{
+		if (num % 10 == 0)
+		{
+
+			++nu;
+		}
+		num /= 10;
+	}
+	
+	cout << "\n Kolichestvo null: " << nu << endl;
+}*/
